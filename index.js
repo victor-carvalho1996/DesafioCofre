38e331c7906a9f80576a4142f0eb220a427f1dc8
victor-carvalho1996 <victorpcarvalho18@gmail.com>
2022-04-27 22:05:44 -0300

    Performance improvement of the problem

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
-/*
-    1: eliminar as possiveis lets
-    2: Adicionar os arrays de teste case
-    3: Todas as variaveis em ingles
-    4: Mudar nome jfinal para JoaoCofre e JoseCofre
-*/
 const testCase = [["20 25","10 5","10 10"],["0 5","12 0","0 20","17 1"]];
 const numberEntries = testCase.length;
-console.log(testCase);
 let JoseEntrie;
 let JoaoEntrie;
 let diferentValue = 0;
