ee3cb1984b9b73ac4c35102528ad1546ccc45720
victor-carvalho1996 <victorpcarvalho18@gmail.com>
2022-04-27 22:04:53 -0300

    Performance improvement of the problem.

--- index.js
+++ index.js
@@ -1,31 +1,27 @@
-function checkCofre() {
-    let quantindadeEntradas = parseInt(prompt("Digite a quantidade de entradas: "));
-    let valor, cofre, zFinal, jFinal, rFinal = [], testeNumero = 0;
+/*
+    1: eliminar as possiveis lets
+    2: Adicionar os arrays de teste case
+    3: Todas as variaveis em ingles
+    4: Mudar nome jfinal para JoaoCofre e JoseCofre
+*/
+const testCase = [["20 25","10 5","10 10"],["0 5","12 0","0 20","17 1"]];
+const numberEntries = testCase.length;
+console.log(testCase);
+let JoseEntrie;
+let JoaoEntrie;
+let diferentValue = 0;
 
-    while (quantindadeEntradas > 0) {
-        testeNumero++;
-        rFinal.push("Teste " + testeNumero);
-        cofre = []
-        for (let index = 0; index < quantindadeEntradas; index++) {
-            valor = prompt("Digite o valor: ");
-    
-            jFinal = parseInt(valor.split(" ")[0]);
-            zFinal = parseInt(valor.split(" ")[1]);
-    
-            if (index > 0) {
-                cofre.push((jFinal - zFinal) + cofre[index - 1]);
-            }else {
-                cofre.push(jFinal - zFinal);
-            }
-            
-        }
-        rFinal = rFinal.concat(cofre);
-        rFinal.push("");
-        quantindadeEntradas = parseInt(prompt("Digite a quantidade de entradas: "));
-    }   
-    let result = "";
-    rFinal.map(element => {
-        result += element + "\n";
-    });
-    console.log(result);
+for (let i = 0; i < numberEntries; i++) {
+    console.log("Teste " + (i + 1) + "\n");
+    diferentValue = 0;
+
+    for (let j = 0; j < testCase[i].length; j++) {
+        JoaoEntrie = parseInt(testCase[i][j].split(" ")[0]);
+        JoseEntrie = parseInt(testCase[i][j].split(" ")[1]);
+
+        diferentValue = (JoaoEntrie - JoseEntrie) + diferentValue;
+
+        console.log(diferentValue + "\n");
+    }
+    console.log("\n");     
 }
